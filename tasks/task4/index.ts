232e45c18e938301319e0034e94dbfaa3c5081f4
Maksym Martynenko <predatar24@gmail.com>
2023-07-26 21:58:21 +0300

    task4

--- tasks/task4/index.ts
+++ tasks/task4/index.ts
@@ -2,38 +2,78 @@ import { stdin, stdout } from 'process';
 import * as readline from 'readline';
 
 class CarNumber {
-  private numbersSet: Set<number> = new Set();
+  private numbersMap: Map<number, string> = new Map();
 
   private readLine = readline.createInterface({ input: stdin, output: stdout });
 
   enterNumber() {
-    this.readLine.question('Car number: ', (value: string) => {
+    this.readLine.question('Car number | User name: ', (value: string) => {
       switch (value.toUpperCase()) {
         case 'СПИСОК':
           this.showAllNumbers();
+          this.enterNumber();
+          break;
+        case 'АВТОМОБИЛИ':
+          this.showCarNumber();
+          this.enterNumber();
+          break;
+        case 'ВЛАДЕЛЬЦЫ':
+          this.showCarUser();
           this.enterNumber();
           break;
         case 'СТОП':
           this.readLine.close();
           break;
         default:
-          this.addNumber(+value);
+          this.addNumber(value);
           this.enterNumber();
           break;
       }
     });
   }
 
-  addNumber(number: number) {
-    if(this.numbersSet.has(number)) {
-			console.log('Есть');
-		}else {
-			this.numbersSet.add(number);
-		}
+  addNumber(number: string) {
+    const mas = number.split(' | ');
+    this.numbersMap.set(+mas[0], mas[1]);
+  }
+
+  changeUser(number: string) {
+    const mas = number.split(' | ');
+    this.numbersMap.set(+mas[0], mas[1]);
   }
 
   showAllNumbers() {
-    this.numbersSet.forEach(elem => console.log(elem));
+    this.numbersMap.forEach((value, key) => console.log(key, value));
+  }
+
+  showCarNumber() {
+    for (const key of this.numbersMap.keys()) {
+      console.log(key);
+    }
+  }
+
+  showCarUser() {
+    const mas = [];
+    for (const user of this.numbersMap.values()) {
+      mas.push(user);
+    }
+    const resMas = this.removeDuplicates(mas);
+    resMas.forEach(elem => console.log(elem));
+  }
+
+  private removeDuplicates(arr: string[]) {
+    const uniqueElements: { [key: string]: boolean } = {};
+    const resultArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      const element = arr[i];
+      if (!uniqueElements[element]) {
+        resultArr.push(element);
+        uniqueElements[element] = true;
+      }
+    }
+
+    return resultArr;
   }
 }
 
