96879f5e479f6d93aa9ee09c4060b66cb70996d9
Maksym Martynenko <predatar24@gmail.com>
2023-07-26 11:32:29 +0300

    map

---
+++ Map/index.ts
@@ -0,0 +1,15 @@
+const map = new Map();
+
+map.set(1,'asd');
+map.set(2,'dasd');
+map.set(2, 'dsa');
+
+console.log(map);
+
+const set = new Set();
+
+set.add('Max');
+set.add('Vlad');
+set.add('Max');
+
+console.log(set);
